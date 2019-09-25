81b229eef5388860de9d6a90be0edba9bf2fd692
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-25 15:04:19 +0300

    add tests for add and all methods implemented from the dao selling interface.

--- src/test/java/models/dao/Sql2oSellingDaoTest.java
+++ src/test/java/models/dao/Sql2oSellingDaoTest.java
@@ -1,17 +1,47 @@
 package models.dao;
 
+import models.Selling;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oSellingDaoTest {
+    private Connection conn;
+    private Sql2oSellingDao sellingDao;
 
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString,"","");
+        sellingDao = new Sql2oSellingDao(sql2o);
+        conn = sql2o.open();
     }
 
     @After
     public void tearDown() throws Exception {
+        conn.close();
+    }
+
+    public Selling setUpNewLand(){
+        Selling selling  = new Selling("Makulos","desc","for lease",100000,"commercial","+453261","Makueni",2);
+        sellingDao.add(selling);
+        return selling;
+    }
+
+    @Test
+    public void addingLandInstanceSetsId_true()throws Exception {
+        assertEquals(1,setUpNewLand().getId());
+    }
+
+    @Test
+    public void all_returnsAllLandEntries()throws Exception {
+        Selling firstLand = setUpNewLand();
+        Selling secondLand = setUpNewLand();
+        assertEquals(2,sellingDao.all().size());
+
     }
 }
