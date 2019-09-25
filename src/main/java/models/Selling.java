66f0ab1e94890a043679f56ab6e4e9541c9c8629
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-25 10:23:56 +0300

    add constructor for selling class.

--- src/main/java/models/Selling.java
+++ src/main/java/models/Selling.java
@@ -1,11 +1,28 @@
 package models;
-//Class Selling (Name, Description, Type, Purpose(Commercial/Residential/Industrial/Special)Location, Size, Price, tel, email)
+
 public class Selling {
     private String land_name;
     private String land_description;
     private String land_type;
-    private float land_price;
+    private double land_price;
     private String purpose;
-    private String tel_no;
+    private String contact;
+    private String land_location;
+    private double size;
+    private int id ;
+
+    public Selling(String land_name,String land_description,String land_type,float land_price,String purpose,String contact,String land_location, float size){
+        this.land_name = land_name;
+        this.land_description = land_description;
+        this.land_type = land_type;
+        this.land_price = land_price;
+        this.purpose = purpose;
+        this.contact = contact;
+        this.land_location = land_location;
+        this.size = size;
+
+    }
+
+
 
 }
