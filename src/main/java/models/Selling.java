dfabc9c44acb3ff389fd2a913109efc26a7a68ca
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-25 11:14:09 +0300

    add getters to class selling.

--- src/main/java/models/Selling.java
+++ src/main/java/models/Selling.java
@@ -4,14 +4,14 @@ public class Selling {
     private String land_name;
     private String land_description;
     private String land_type;
-    private double land_price;
+    private int land_price;
     private String purpose;
     private String contact;
     private String land_location;
-    private double size;
+    private int size;
     private int id ;
 
-    public Selling(String land_name,String land_description,String land_type,float land_price,String purpose,String contact,String land_location, float size){
+    public Selling(String land_name,String land_description,String land_type,int land_price,String purpose,String contact,String land_location, int size){
         this.land_name = land_name;
         this.land_description = land_description;
         this.land_type = land_type;
@@ -23,6 +23,39 @@ public class Selling {
 
     }
 
+    public String getLand_name() {
+        return land_name;
+    }
 
+    public String getLand_description() {
+        return land_description;
+    }
 
+    public String getLand_type() {
+        return land_type;
+    }
+
+    public int getLand_price() {
+        return land_price;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getLand_location() {
+        return land_location;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
