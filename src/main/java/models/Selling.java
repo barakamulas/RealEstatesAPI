3399748ff4784f2af8167a9165c837dc0682c6a1
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-25 09:07:01 +0300

    add fields in the selling class.

--- src/main/java/models/Selling.java
+++ src/main/java/models/Selling.java
@@ -1,4 +1,11 @@
 package models;
-
+//Class Selling (Name, Description, Type, Purpose(Commercial/Residential/Industrial/Special)Location, Size, Price, tel, email)
 public class Selling {
+    private String land_name;
+    private String land_description;
+    private String land_type;
+    private float land_price;
+    private String purpose;
+    private String tel_no;
+
 }
