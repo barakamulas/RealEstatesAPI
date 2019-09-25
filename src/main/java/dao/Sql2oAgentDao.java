7be6ec41bf66fe2250bb56483d0ba221c65b350d
WendyOmollo <omollow280@gmail.com>
2019-09-25 12:37:16 +0300

    Add a test for interface implementation class

--- src/main/java/dao/Sql2oAgentDao.java
+++ src/main/java/dao/Sql2oAgentDao.java
@@ -1,4 +1,33 @@
 package dao;
 
-public class Sql2oAgentDao {
+import models.Agent;
+
+import java.util.List;
+
+public class Sql2oAgentDao implements AgentDao {
+
+    @Override
+    public void add(Agent agent) {
+
+    }
+
+    @Override
+    public List<Agent> getAgents() {
+        return null;
+    }
+
+    @Override
+    public Agent findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void clearAll() {
+
+    }
 }
