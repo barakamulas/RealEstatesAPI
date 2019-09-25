00095e7b21009fca1d4826d104f0593637cac89e
WendyOmollo <omollow280@gmail.com>
2019-09-25 10:01:57 +0300

    Change naming convention for contact property for consistency

--- src/main/java/models/Agent.java
+++ src/main/java/models/Agent.java
@@ -2,12 +2,12 @@ package models;
 
 public class Agent {
     private String agent_name;
-    private String agent_contact;
+    private String contact;
     private String agent_reviews;
     private int id;
 
-    public Agent(String agent_name,String agent_contact,String agent_reviews) {
-        this.agent_contact = agent_contact;
+    public Agent(String agent_name,String contact,String agent_reviews) {
+        this.contact = contact;
         this.agent_name = agent_name;
         this.agent_reviews = agent_reviews;
         this.id = id;
