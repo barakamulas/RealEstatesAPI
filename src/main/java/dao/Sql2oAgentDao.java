ce388a26432e43a4d852aea19a6ffc526c6b9396
WendyOmollo <omollow280@gmail.com>
2019-09-25 14:19:42 +0300

    Add necessary dependencies for tests to run

--- src/main/java/dao/Sql2oAgentDao.java
+++ src/main/java/dao/Sql2oAgentDao.java
@@ -1,14 +1,28 @@
 package dao;
 
 import models.Agent;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
 public class Sql2oAgentDao implements AgentDao {
+    private final Sql2o sql2o;
+    public Sql2oAgentDao(Sql2o sql2o) { this.sql2o = sql2o; }
 
     @Override
     public void add(Agent agent) {
-
+        String sql = "INSERT INTO agents(agent_name,contact,agent_reviews) VALUES (:agent_name,:contact,:agent_reviews)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(agent)
+                    .executeUpdate()
+                    .getKey();
+           agent.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
