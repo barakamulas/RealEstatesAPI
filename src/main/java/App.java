c10f71b01ffc16946f59b4701516f490fd46c6c7
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-26 12:26:26 +0300

    add route to index.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,9 +9,15 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import static spark.Spark.*;
 import com.google.gson.Gson;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
+      staticFileLocation("/public");
       Sql2oBuiltDao builtDao;
       Sql2oSellingDao sellingDao;
       Sql2oAgentDao agentDao;
@@ -24,5 +30,10 @@ public class App {
         builtDao = new Sql2oBuiltDao(sql2o);
         sellingDao = new Sql2oSellingDao(sql2o);
         agentDao = new Sql2oAgentDao(sql2o);
+
+        get("/",(req,res)->{
+          Map<String,Object> model = new HashMap<>();
+          return new ModelAndView(model,"index.hbs");
+        },new HandlebarsTemplateEngine());
     }
 }
