d334ccb4b15804c2f58701daae793427972b9482
barakamulas <barakafrank18@gmail.com>
2019-09-26 14:52:00 +0300

    Write test for the add and getAll methods in AgentBuiltDao interface and implement them in Sql2oAgentBuiltDao class

--- src/main/java/models/dao/AgentBuiltDao.java
+++ src/main/java/models/dao/AgentBuiltDao.java
@@ -1,5 +1,6 @@
 package models.dao;
 
+import models.AgentBuilt;
 import models.Built;
 
 import java.util.List;
@@ -7,9 +8,9 @@ import java.util.List;
 public interface AgentBuiltDao {
 
 
-//    void add(Built built);
+    void add(AgentBuilt agentbuilt);
 
-//    List<Built> getAll();
+    List<AgentBuilt> getAll();
 //
 //    Built findById(int id);
 //
--- src/test/java/models/dao/Sql2oAgentBuiltDaoTest.java
+++ src/test/java/models/dao/Sql2oAgentBuiltDaoTest.java
@@ -1,17 +1,110 @@
 package models.dao;
 
+import models.AgentBuilt;
+import models.Built;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oAgentBuiltDaoTest {
 
+    private Connection conn;
+    private models.dao.Sql2oAgentBuiltDao agentBuiltDao;
+
+
     @Before
     public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        agentBuiltDao = new models.dao.Sql2oAgentBuiltDao(sql2o);
+        conn = sql2o.open();
     }
 
     @After
     public void tearDown() throws Exception {
+        conn.close();
     }
+
+    @Test
+    public void addingAgentBuiltSetsId() throws Exception {
+        AgentBuilt testAgentBuilt = setupAgentBuilt();
+        assertNotEquals(0, testAgentBuilt.getId());
+    }
+
+    @Test
+    public void addedAgentBuiltsAreReturnedFromGetAll() throws Exception {
+        AgentBuilt testAgentBuilt = setupAgentBuilt();
+        assertEquals(1, agentBuiltDao.getAll().size());
+    }
+
+//    @Test
+//    public void noBuiltsReturnsEmptyList() throws Exception {
+//        assertEquals(0, agentBuiltDao.getAll().size());
+//    }
+//
+//    @Test
+//    public void findByIdReturnsCorrectBuilt() throws Exception {
+//        AgentBuilt testAgentBuilt = setupAgentBuilt();
+//        AgentBuilt otherAgentBuilt = setupAgentBuilt();
+//        assertEquals(testAgentBuilt , agentBuiltDao.findById(testAgentBuilt .getId()));
+//    }
+//
+//
+//    @Test
+//    public void deleteByIdDeletesCorrectBuilt() throws Exception {
+//        AgentBuilt testAgentBuilt = setupAgentBuilt();
+//        AgentBuilt otherAgentBuilt = setupAltBuilt();
+//        agentBuiltDao.deleteById(testAgentBuilt .getId());
+//        assertEquals(1, agentBuiltDao.getAll().size());
+//    }
+//
+//    @Test
+//    public void clearAll() throws Exception {
+//        AgentBuilt testAgentBuilt = setupAgentBuilt();
+//        AgentBuilt otherAgentBuilt = setupAgentBuilt();
+//        agentBuiltDao.clearAll();
+//        assertEquals(0, agentBuiltDao.getAll().size());
+//    }
+//
+//
+//    @Test
+//    public void updateCorrectlyUpdatesAllFields() throws Exception {
+//        AgentBuilt testAgentBuilt = setupAgentBuilt();
+//        agentBuiltDao.update(testAgentBuilt .getId(), "a", "b", "c", 1000000, "For Sale", "Commercial", "+254744444444");
+//        AgentBuilt foundAgentBuilt = agentBuiltDao.findById(testAgentBuilt .getId());
+//        assertEquals("a", foundAgentBuilt .getBuilt_name());
+//        assertEquals("b", foundAgentBuilt .getBuilt_description());
+//        assertEquals("c", foundAgentBuilt .getBuilt_location());
+//        assertEquals("For Sale", foundAgentBuilt .getType());
+//        assertEquals("Commercial", foundAgentBuilt .getPurpose());
+//        assertEquals("+254744444444", foundAgentBuilt .getContact());
+//    }
+
+
+    public AgentBuilt setupAgentBuilt (){
+        AgentBuilt testAgentBuilt = new AgentBuilt("Kasuku Centre Lounge", "Single floor Bar and Lounge", "Kileleshwa", 20000000, "For Sale", "Commercial","+254722222222",1);
+        agentBuiltDao.add(testAgentBuilt);
+        return testAgentBuilt;
+    }
+
+    public AgentBuilt setupAltAgentBuilt (){
+        AgentBuilt altAgentBuilt = new AgentBuilt("Kasuku Centre Lounge", "Single floor Bar and Lounge", "Kileleshwa", 20000000, "For Sale", "Commercial","+254722222222",1);
+        agentBuiltDao.add(altAgentBuilt);
+        return altAgentBuilt;
+    }
+
+    public AgentBuilt setupThirdAgentBuilt (){
+        AgentBuilt thirdAgentBuilt = new AgentBuilt("Kasuku Centre Lounge", "Single floor Bar and Lounge", "Kileleshwa", 20000000, "For Sale", "Commercial","+254722222221",1);
+        agentBuiltDao.add(thirdAgentBuilt);
+        return thirdAgentBuilt;
+
+    }
+
+
+
+
 }
